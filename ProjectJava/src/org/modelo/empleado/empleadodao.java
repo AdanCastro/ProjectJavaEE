0617fab9751d03c141a83725428e843e6dd17fe1
ramon.castro <adancastrolml@gmail.com>
2020-05-16 15:16:28 -0500

    se agregaron comentario

--- ProjectJava/src/org/modelo/empleado/empleadodao.java
+++ ProjectJava/src/org/modelo/empleado/empleadodao.java
@@ -3,6 +3,23 @@ package org.modelo.empleado;
 import org.modelo.empleado.Empleado;
 
 public class empleadodao {
-
+ /*
+  * asd
+  * asd
+  * sa
+  * dd
+  * cvb
+  * cvb
+  * cb
+  * cb
+  * vcv
+  * bvb
+  * vcbcv
+  * bc
+  * w
+  * er
+  * werwe
+  * r
+  */
 
 }
