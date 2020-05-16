3eb0fd552464671a5232ac0973e98c179f3815a5
ramon.castro <adancastrolml@gmail.com>
2020-05-16 15:22:13 -0500

    se agregó clase controller

---
+++ ProjectJava/src/org/modelo/empleado/empleadocontroller.java
@@ -0,0 +1,20 @@
+package org.modelo.empleado;
+
+public class empleadocontroller {
+
+	/*
+	 * 
+	 * pruebas de commits
+	 * en github
+	 * proyecto javaEE
+	 * 1
+	 * 2
+	 * 3
+	 * 4
+	 * 5
+	 * 6
+	 * 10
+	 * 
+	 * 
+	 */
+}
